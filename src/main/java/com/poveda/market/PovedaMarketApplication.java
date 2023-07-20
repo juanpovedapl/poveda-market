a65bb3e9b1e4132894941e5998216241a5d74844
juanpovedapl <juanpablopoveda93@gmail.com>
2023-07-20 15:31:54 -0500

    Proyecto de base inicializado con spring y gradle

---
+++ src/main/java/com/poveda/market/PovedaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.poveda.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovedaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PovedaMarketApplication.class, args);
+	}
+
+}
